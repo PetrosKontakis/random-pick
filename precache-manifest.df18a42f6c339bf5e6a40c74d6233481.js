39bd0f35f6df5c8d403ab5dcfa4e15c267a2c2e2
petros_ <p.kontakis@athlenda.com>
2019-12-12 18:53:43 +0200

    Updates

---
+++ precache-manifest.df18a42f6c339bf5e6a40c74d6233481.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3596cde67513057b62af6d3201c44a7",
+    "url": "/random-pick/index.html"
+  },
+  {
+    "revision": "54e6fcde1ab344553b4c",
+    "url": "/random-pick/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "ecb4042ca5740bd719e9",
+    "url": "/random-pick/static/css/main.e31d3e9c.chunk.css"
+  },
+  {
+    "revision": "54e6fcde1ab344553b4c",
+    "url": "/random-pick/static/js/2.1ac1699f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/random-pick/static/js/2.1ac1699f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ecb4042ca5740bd719e9",
+    "url": "/random-pick/static/js/main.3da6e2d5.chunk.js"
+  },
+  {
+    "revision": "b5365a868419aacd150d",
+    "url": "/random-pick/static/js/runtime-main.3f6330ef.js"
+  },
+  {
+    "revision": "6a19414b79089e03ff6d46cd537369f2",
+    "url": "/random-pick/static/media/clear-24px.6a19414b.svg"
+  }
+]);
